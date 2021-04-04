1c46bbc69cad407f9fa2eec2161e96adabccf173
Aline Lanckneus <aline@isleof.it>
2021-04-04 16:38:02 +0200

    render map

--- front-end-test/src/App.js
+++ front-end-test/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
+import "mapbox-gl/dist/mapbox-gl.css";
+import React from "react";
+import ReactMapboxGl, {Feature, Layer} from "react-mapbox-gl";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
+import "./App.css";
 
+mapboxgl.workerClass = MapboxWorker;
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWxpbmVsYW5ja25ldXMiLCJhIjoiY2tuMzhoemNkMDVxbDJwcWIxa2M4cnh5bCJ9.anBAvGXMPqYH_hG3kDEq4w";
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiYWxpbmVsYW5ja25ldXMiLCJhIjoiY2tuMzhoemNkMDVxbDJwcWIxa2M4cnh5bCJ9.anBAvGXMPqYH_hG3kDEq4w",
+});
+const RenderMap = () => {
+  return (
+    <Map
+      style='mapbox://styles/mapbox/streets-v9'
+      containerStyle={{
+        height: "100vh",
+        width: "100vw",
+      }}>
+      <Layer type='symbol' id='marker' layout={{"icon-image": "marker-15"}}>
+        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+      </Layer>
+    </Map>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RenderMap />
     </div>
   );
 }
