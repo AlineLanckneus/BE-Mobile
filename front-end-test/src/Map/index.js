7320dec5ee35e33970f40083cc30d2f1de414f30
Aline Lanckneus <aline@isleof.it>
2021-04-04 17:54:49 +0200

    wip add markers

--- front-end-test/src/Map/index.js
+++ front-end-test/src/Map/index.js
@@ -4,6 +4,33 @@ import {StyledMapContainer, StyledSidebar} from "./styles";
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiYWxpbmVsYW5ja25ldXMiLCJhIjoiY2tuMzhoemNkMDVxbDJwcWIxa2M4cnh5bCJ9.anBAvGXMPqYH_hG3kDEq4w";
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+const geojson = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.032, 38.913],
+      },
+      properties: {
+        title: "Mapbox",
+        description: "Washington, D.C.",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-122.414, 37.776],
+      },
+      properties: {
+        title: "Mapbox",
+        description: "San Francisco, California",
+      },
+    },
+  ],
+};
 
 const Map = () => {
   const mapContainerRef = useRef(null);
@@ -29,7 +56,15 @@ const Map = () => {
       setLat(map.getCenter().lat.toFixed(4));
       setZoom(map.getZoom().toFixed(2));
     });
+    // add markers to map
+    geojson.features.forEach(function (marker) {
+      // create a HTML element for each feature
+      var el = document.createElement("div");
+      el.className = "marker";
 
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+    });
     // Clean up on unmount
     return () => map.remove();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
