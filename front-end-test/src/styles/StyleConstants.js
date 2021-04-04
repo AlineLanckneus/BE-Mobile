156c89c9a1c5a7bbe468be99399a9a85c742cf3c
Aline Lanckneus <aline@isleof.it>
2021-04-04 17:45:51 +0200

    create styleconstants

---
+++ front-end-test/src/styles/StyleConstants.js
@@ -0,0 +1,30 @@
+export const COLORS = {
+  white: "#fff",
+};
+export const FONT_SIZES = {
+  baseSize: "1em",
+  scaleRatio: 1.25,
+  xs: "0.8em",
+  s: "1.25em",
+  m: "1.563em",
+  l: "1.953em",
+  xl: "2.441em",
+  xxl: "3.052em",
+};
+export const FONT_WEIGHTS = {
+  light: 100,
+  regular: 400,
+  semiBold: 600,
+  bold: 700,
+};
+
+export const HEIGHTS = {};
+export const SPACINGS = {
+  xxs: "4px",
+  xs: "8px",
+  s: "16px",
+  m: "24px",
+  l: "32px",
+  xl: "40px",
+  xxl: "48px",
+};
