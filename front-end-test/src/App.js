17c64bcfbdbead82d435a80deb33e1994006319a
Aline Lanckneus <aline@isleof.it>
2021-04-05 13:27:58 +0200

    get data in useEffect on mount

--- front-end-test/src/App.js
+++ front-end-test/src/App.js
@@ -1,14 +1,29 @@
+import axios from "axios";
 import "mapbox-gl/dist/mapbox-gl.css";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import Map from "./Map/index";
 
-function App() {
+const App = () => {
+  const [records, setRecords] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://data.stad.gent/api/records/1.0/search/?dataset=real-time-bezettingen-fietsenstallingen-gent&q=&facet=facilityname"
+      )
+      .then((response) => setRecords(response.data.records))
+      .catch((error) => console.log(error.message));
+  }, [records.length]);
   return (
     <div>
+      <div>
+        {records.map((r) => (
+          <div>{r.geometry.coordinates}</div>
+        ))}
+      </div>
       <Map />
     </div>
   );
-}
+};
 
 export default App;
